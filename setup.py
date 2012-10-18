02511a0c4b68356000f33f1ad5dc778b67bd1d53
nvbn <nvbn.rm@gmail.com>
2012-10-19 02:39:47 +0400

    fix 12.10

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(name='vkvideo',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          "vkontakte"
       ],
       entry_points={
         'console_scripts': [
